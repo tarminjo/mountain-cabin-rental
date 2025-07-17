702cd7de711cfa5a971aa41353bba920cb547643
Mitar Arsovic <mitararsovic001@gmail.com>
2025-07-17 11:36:23 +0200

    BE: add end-point for accept/decline registration request

--- backend/src/main/java/com/example/backend/controllers/AdminController.java
+++ backend/src/main/java/com/example/backend/controllers/AdminController.java
@@ -2,6 +2,7 @@ package com.example.backend.controllers;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,14 @@ public class AdminController {
         return new AdminRepository().login(admin.getUsername(), admin.getPassword());
     }
 
-    //TODO: add end-point for accept/decline user registration
+    @PostMapping("/accept-registration")
+    public void acceptRegistration(@RequestAttribute String username) {
+        new AdminRepository().acceptRegistrationRequest(username);
+    }
 
+    @PostMapping("/decline-registration")
+    public void declineRegistration(@RequestAttribute String username) {
+        new AdminRepository().declineRegistrationRequest(username);
+    }
     
 }
