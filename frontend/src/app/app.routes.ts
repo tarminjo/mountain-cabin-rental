ec141f96062d4fa0d829a2903e53244fb03c5ee0
Mitar Arsovic <mitararsovic001@gmail.com>
2025-07-19 21:09:14 +0200

    FE: nav bar for owner. add routest to front

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -9,6 +9,11 @@ import { OwnerComponent } from './owner/owner.component';
 import { TouristComponent } from './tourist/tourist.component';
 import { AdminUsersDashboardComponent } from './admin-users-dashboard/admin-users-dashboard.component';
 import { AdminCabinsDashboardComponent } from './admin-cabins-dashboard/admin-cabins-dashboard.component';
+import { OwnerRentalsComponent } from './owner-rentals/owner-rentals.component';
+import { OwnerCabinsComponent } from './owner-cabins/owner-cabins.component';
+import { OwnerStatisticsComponent } from './owner-statistics/owner-statistics.component';
+import { TouristCabinsComponent } from './tourist-cabins/tourist-cabins.component';
+import { TouristRentalsComponent } from './tourist-rentals/tourist-rentals.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
@@ -19,5 +24,10 @@ export const routes: Routes = [
     {path: 'owner', component: OwnerComponent},
     {path: 'tourist', component: TouristComponent},
     {path: 'admin-users-dashboard', component: AdminUsersDashboardComponent},
-    {path: 'admin-cabins-dashboard', component: AdminCabinsDashboardComponent}
+    {path: 'admin-cabins-dashboard', component: AdminCabinsDashboardComponent},
+    {path: 'owner-rentals', component: OwnerRentalsComponent},
+    {path: 'owner-cabins', component: OwnerCabinsComponent},
+    {path: 'owner-statistics', component: OwnerStatisticsComponent},
+    {path: 'tourist-cabins', component: TouristCabinsComponent},
+    {path: 'tourist-reservations', component: TouristRentalsComponent}
 ];
--- frontend/src/app/owner/owner.component.ts
+++ frontend/src/app/owner/owner.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-owner',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './owner.component.html',
   styleUrl: './owner.component.css'
 })
