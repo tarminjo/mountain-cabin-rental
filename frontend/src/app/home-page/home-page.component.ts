936572947aceec5e5d366f6a6bfc19aeb2ae0736
Mitar Arsovic <mitararsovic001@gmail.com>
2025-07-22 20:09:54 +0200

    FE: add home page. fix buttons to info

--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { CabinService } from '../services/cabin.service';
+import { Cabin } from '../models/cabin';
 
 @Component({
   selector: 'app-home-page',
@@ -12,82 +14,96 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent implements OnInit {
-  constructor(private userService: UserService) {}
-  
+
+  constructor(private router: Router, private userService: UserService,
+    private cabinService: CabinService
+  ) {}
+
+  username: string = localStorage.getItem("logged") || ""
+  cabins: Cabin[] = []
+  originalCabins: Cabin[] = []
+
   ngOnInit(): void {
-    // this.userService.getOwnerNumber().subscribe((num: number)=>{
-    //   this.ownerNumber = num
-    // })
+      
+    this.nameParam = ""
+    this.locationParam = ""
 
-    // this.userService.getDecoratorNumber().subscribe((num: number)=>{
-    //   this.decoratorNumber=num
-    // })
-
-    // this.userService.getAllCompanies().subscribe((companies: Company[])=>{
-    //   this.listOfCompanies = companies
-    // })
-
-    // //deo za ukupan broj uredjenih basti
-    // this.userService.getNumOfDoneGardens().subscribe((jobs: Job[])=>{
-    //   this.gardenNumber = jobs.length
-    // })
-
-    // //Deo za ukupan broj poslova u poslednjih 24 sata
-    // this.userService.getNumOfJobsLast24Hours().subscribe((jobs: Job[])=>{
-    //   this.numberOfJobsLast24Hours = jobs.length
-    // })
-
-    // //Deo za ukupan broj poslova u poslednjih 7 dana
-    // this.userService.getNumOfJobsLast7Days().subscribe((jobs: Job[])=>{
-    //   this.numberOfJobsLast7Days = jobs.length
-    // })
-    // //Deo za ukupan broj poslova u poslednjih 30 dana
-    // this.userService.getNumOfJobsLast30Days().subscribe((jobs: Job[])=>{
-    //   this.numberOfJobsLast30Days = jobs.length
-    // })
-
-
-
-    // this.nameParam = ""
-    // this.addressParam = ""
-
+    this.cabinService.getAllCabins().subscribe((myCabins: Cabin[])=>{
+      this.cabins = myCabins
+      this.originalCabins = myCabins
+    })
   }
 
-  //listOfCompanies: Company[] = []
+  error: boolean = false
+  message: string = ""
 
-  // ownerNumber: number
-  // decoratorNumber: number
-  // gardenNumber: number
-  // numberOfJobsLast24Hours: number
-  // numberOfJobsLast7Days: number
-  // numberOfJobsLast30Days: number
+  logout() {
+    localStorage.removeItem('logged');
+    this.router.navigate(['/login']);
+  }
 
   nameParam: string = ""
-  addressParam: string = ""
+  locationParam: string = ""
 
-  // search(){
-  //   this.userService.searchCompany(this.nameParam, this.addressParam).subscribe((companies: Company[])=>{
-  //     this.listOfCompanies = companies
-  //   })
-  // }
+  activeNameSort: 'asc' | 'desc' | null = null;
+  activeLocationSort: 'asc' | 'desc' | null = null;
 
-  // cancelSearch(){
-  //   this.ngOnInit()
-  // }
-  
-  // sortAscedingName(){
-  //   this.listOfCompanies.sort((a, b)=>(a.name).localeCompare(b.name))
-  // }
+  applySorting() {
+    this.cabins = [...this.originalCabins];
 
-  // sortDescedingName(){
-  //   this.listOfCompanies.sort((a, b)=>(b.name).localeCompare(a.name))
-  // }
+    if (this.activeNameSort) {
+      this.cabins.sort((a, b) =>
+        this.activeNameSort === 'asc'
+          ? a.name.localeCompare(b.name)
+          : b.name.localeCompare(a.name)
+      );
+    }
 
-  // sortAscedingAddress(){
-  //   this.listOfCompanies.sort((a, b)=>(a.address).localeCompare(b.address))
-  // }
+    if (this.activeLocationSort) {
+      this.cabins.sort((a, b) =>
+        this.activeLocationSort === 'asc'
+          ? a.location.localeCompare(b.location)
+          : b.location.localeCompare(a.location)
+      );
+    }
+  }
 
-  // sortDescedingAddress(){
-  //   this.listOfCompanies.sort((a, b)=>(b.address).localeCompare(a.address))
-  // }
+  toggleNameSort(order: 'asc' | 'desc') {
+    this.activeNameSort = this.activeNameSort === order ? null : order;
+    this.applySorting();
+  }
+
+  toggleLocationSort(order: 'asc' | 'desc') {
+    this.activeLocationSort = this.activeLocationSort === order ? null : order;
+    this.applySorting();
+  }
+
+  resetSortAndFilters() {
+    this.activeNameSort = null;
+    this.activeLocationSort = null;
+    this.locationParam = '';
+    this.nameParam = '';
+    this.cabins = [...this.originalCabins];
+  }
+
+  search(){
+
+    if (!this.nameParam && !this.locationParam) {
+      this.cabins = this.originalCabins
+      return;
+    }
+
+    this.cabinService.searchCabins(this.nameParam, this.locationParam).subscribe((cabins: Cabin[])=>{
+      this.cabins = cabins
+    })
+  }
+
+  totalCabins: number = 0
+  totalOwners: number = 0
+  totalTourists: number = 0
+  reservedLast24h: number = 0
+  reservedLast7d: number = 0
+  reservedLast30d: number = 0
+
+  // Method to fetch statistics
 }
